f2fab62b7144b1e32b100c16b0e178a3418fd652
Guilherme Barone Milani <guibarone.milani@gmail.com>
2026-04-23 11:43:02 -0300

    Update Banco.java

--- Banco.java
+++ Banco.java
@@ -10,6 +10,20 @@ public class Banco {
     private String senha;
     private String chave;
 
+
+    public String getConta(){
+        return numeroContaBancaria + "-" + digito;
+    }
+
+    public double getSaldoConta() {
+        return saldoConta;
+    }
+
+    public String getChave(){
+        return chave;
+    }
+
+
     public Banco(String nomeTitular, String senha, String chave){
         this.nomeTitular = nomeTitular;
         this.senha = senha;
@@ -20,21 +34,16 @@ public class Banco {
         this.chave = chave;
     }
 
-    public String getConta(){
-        return numeroContaBancaria + "-" + digito;
-    }
 
     public boolean autenticar(String senhaDigitada){
         return this.senha.equals(senhaDigitada);
     }
 
-    public String getChave(){
-        return this.chave;
-    }
 
     private int calcularDigito(int numero) {
         return numero % 10;
     }
+
 
     public void depositar(double valor) {
         if (valor > 0) {
@@ -45,8 +54,9 @@ public class Banco {
             System.out.println("Valor inválido! ");
     }
 
+
     public boolean sacar(double valor){
-        if (valor > saldoConta) {
+        if (valor > getSaldoConta()) {
             System.out.println("Não foi possível fazer a retirada\nSaldo insuficiente! ");
             return false;
 
@@ -60,27 +70,35 @@ public class Banco {
         return true;
     }
 
+
+    public void receberTransferencia(double valor) {
+        this.saldoConta += valor;
+    }
+
     public boolean transferir(Banco destino, double valor) {
-        if (valor <= 0) {
+        if (valor <= 0 || valor > 5000) {
             System.out.println("Valor inválido!");
             return false;
 
-        }if(valor > saldoConta) {
+        }if(valor > getSaldoConta()) {
             System.out.println("Saldo insuficiente!");
             return false;
 
-        }if (this == destino)
+        }if (this == destino) {
             System.out.println("Não pode transferir para si mesmo");
+            return false;
+        }
 
 
         this.saldoConta -= valor;
-        destino.saldoConta += valor;
+        destino.receberTransferencia(valor);
 
         System.out.println("Transferência realizada com sucesso!");
         return true;
     }
 
+
     public void mostrarInformacaoConta(){
-        System.out.printf("Titular: %s\nAgência: %d\nConta: %s\nChave: %s\nSaldo: %.2f", nomeTitular, agencia, getConta(), getChave(), saldoConta);
+        System.out.printf("Titular: %s\nAgência: %d\nConta: %s\nChave: %s\nSaldo: %.2f", nomeTitular, agencia, getConta(), getChave(), getSaldoConta());
     }
 }
